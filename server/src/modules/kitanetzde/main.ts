4e15784063fdf4f36c046444f6d75136268b6d95
Eyal Ben Ivri <eyalbenivri@gmail.com>
2018-05-27 21:21:09 +0200

    some packages update, continued with BL

--- server/src/modules/kitanetzde/main.ts
+++ server/src/modules/kitanetzde/main.ts
@@ -2,30 +2,46 @@ import {KitaNetzLANDOpts} from "./landopts";
 import * as path from "path";
 import * as rp from "request-promise-native";
 import * as cheerio from 'cheerio';
+import {Promise} from "es6-promise";
+
+class KitaNetzDEPageResults {
+    constructor(pageNumber:number, hasNextPage: boolean, kitaLink: [string]) {}
+}
 
 export class KitaNetzDE {
     // http://www.kitanetz.de/kitasuche/kita-suchergebnis.php?t_suchwort=lichtenberg&t_plz=&t_stadt=&t_land=
     baseUrl = "http://www.kitanetz.de/kitasuche/kita-suchergebnis.php";
 
-    search(keyword: string = null, plz: number = null, city: string = null, land: KitaNetzLANDOpts = null) {
+    search(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts) {
+        const links = [];
+        this.getPage(keyword, plz, city, land, 1).then();
+    };
+
+    getPage(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts, pageNumber: number): Promise<KitaNetzDEPageResults> {
         const options = {
             uri: this.baseUrl,
             qs: {
                 t_suchwort: keyword || "",
                 t_plz: plz || "",
                 t_stadt: city || "",
-                t_land: land || ""
+                t_land: land || "",
+                go: pageNumber
             },
             transform: body => cheerio.load(body)
-        }
-
-        rp(options).then(($) => {
-            const links = $("#inhalt").children("a").map((i, el) => {
-                return path.join(this.baseUrl, $(el).attr("href"));
-            }).get();
-            console.log(links);
-        }).catch((err) => {
-            console.error(err);
+        };
+        return new Promise((resolve, reject) => {
+            rp(options).then(($) => {
+                const links = $("#inhalt").children("a").filter((i, el) => {
+                    return $(el).attr("title") != "Home";
+                }).map((i, el) => {
+                    return path.join(this.baseUrl, $(el).attr("href"));
+                }).get();
+                const hasNextLink = $("#navigation").children("a").filter((i, el) => el.html() == " » ").length
+                return resolve(new KitaNetzDEPageResults(pageNumber, hasNextLink, links));
+            }).catch((err) => {
+                console.error(err);
+                reject(err);
+            });
         });
     }
 }
