d4b6daf2e281a43eee6f21dfb46cfc2fd316b947
Eyal Ben Ivri <eyalbenivri@gmail.com>
2018-05-30 10:17:55 +0200

    temp. won't compile

--- server/src/modules/kitanetzde/main.ts
+++ server/src/modules/kitanetzde/main.ts
@@ -3,26 +3,47 @@ import * as path from "path";
 import * as rp from "request-promise-native";
 import * as cheerio from 'cheerio';
 import {Promise} from "es6-promise";
+import { doWhilst, queue } from 'async';
 
 class KitaNetzDEPageResults {
     constructor(public pageNumber:number, public hasNextPage: boolean, public kitaLinks: [string]) {}
 }
 
+class KitaResult {
+    constructor(public email:string, public phoneNumber:string, public address: string) {}
+}
+
 export class KitaNetzDE {
     // http://www.kitanetz.de/kitasuche/kita-suchergebnis.php?t_suchwort=lichtenberg&t_plz=&t_stadt=&t_land=
     baseUrl = "http://www.kitanetz.de/kitasuche/kita-suchergebnis.php";
+    constructor(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts) {
+        // define a q to process the search page results
+        this.searchResultsQ = queue(this.searchPage, 4);
 
-    search(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts) {
-        const links = [];
-        let hasNextPage = true;
-        while(hasNextPage) {
-            this.getPage(keyword, plz, city, land, 1).then((pageResults:KitaNetzDEPageResults) => {
-                if (pageResults.kitaLinks.length > 0) {
-                    links.concat(pageResults.kitaLinks);
-                }
-                hasNextPage = pageResults.hasNextPage;
-            });
-        }
+
+        // define a q to process each result
+        const kitaPagesQ = queue(this.parseKitaPage, 4);
+    }
+
+    search(): Promise<[KitaResult]> {
+        return new Promise((resolve, reject) => {
+            const results: [KitaResult] = [];
+
+
+            q.pause();
+            q.drain = () => { resolve(results) };
+            let pageNumber = 1;
+            doWhilst((callback) => {
+                this.getPage(keyword, plz, city, land, pageNumber++)
+                    .then((pageResults:KitaNetzDEPageResults) => {
+                        if (pageResults.kitaLinks.length > 0) {
+                            pageResults.kitaLinks.forEach((url) => q.push({url}));
+                        }
+                        callback();
+                    });
+            }, (results) => results.last().hasNextPage)
+        });
+
     };
 
     getPage(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts, pageNumber: number): Promise<KitaNetzDEPageResults> {
