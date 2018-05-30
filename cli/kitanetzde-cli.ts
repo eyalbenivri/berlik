cdd09ce45b908022aeeb92f4e205e6f0410cabb2
Eyal Ben Ivri <eyalbenivri@gmail.com>
2018-05-31 01:15:09 +0200

    got to kita pages... started parsing

--- cli/kitanetzde-cli.ts
+++ cli/kitanetzde-cli.ts
@@ -1,6 +1,6 @@
 import { KitaNetzDE } from "../server/src/modules/kitanetzde/main";
 import * as prettyjson from 'prettyjson';
 
-const kitanetztool = new KitaNetzDE();
-kitanetztool.search(null, 10247, null, null)
+const kitanetztool = new KitaNetzDE(null, 10247, null, null);
+kitanetztool.search()
     .then(data => console.log(prettyjson.render(data)));
--- server/src/modules/kitanetzde/main.ts
+++ server/src/modules/kitanetzde/main.ts
@@ -3,11 +3,11 @@ import * as path from "path";
 import * as rp from "request-promise-native";
 import * as cheerio from 'cheerio';
 import {Promise} from "es6-promise";
-import { doWhilst, queue } from 'async';
+import {AsyncQueue, queue} from 'async';
 
-class KitaNetzDEPageResults {
-    constructor(public pageNumber:number, public hasNextPage: boolean, public kitaLinks: [string]) {}
-}
+// class KitaNetzDEPageResults {
+//     constructor(public pageNumber:number, public hasNextPage: boolean, public kitaLinks: [string]) {}
+// }
 
 class KitaResult {
     constructor(public email:string, public phoneNumber:string, public address: string) {}
@@ -15,63 +15,108 @@ class KitaResult {
 
 export class KitaNetzDE {
     // http://www.kitanetz.de/kitasuche/kita-suchergebnis.php?t_suchwort=lichtenberg&t_plz=&t_stadt=&t_land=
-    baseUrl = "http://www.kitanetz.de/kitasuche/kita-suchergebnis.php";
-    constructor(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts) {
-        // define a q to process the search page results
-        this.searchResultsQ = queue(this.searchPage, 4);
+    private static baseUrl = "http://www.kitanetz.de/kitasuche/kita-suchergebnis.php";
+    private static baseUrlForKita = "http://www.kitanetz.de/";
+    private searchResultsQ:AsyncQueue<{pageNumber:number}> = null;
+    private kitaPagesQ:AsyncQueue<{kitaUrl:string, pageNumber:number}> = null;
+    private queuedPages:number[] = [];
+    private results:KitaResult[] = [];
 
+    private keyword:string;
+    private plz:number;
+    private city:string;
+    private land:KitaNetzLANDOpts;
+
+    constructor(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts) {
+        this.keyword = keyword;
+        this.plz = plz;
+        this.city = city;
+        this.land = land;
+
+        // define a q to process the search page results
+        this.searchResultsQ = queue<{pageNumber:number}, object>((task, callback) =>
+            this.getSearchPage(this.keyword, this.plz, this.city, this.land, task.pageNumber, callback), 4);
+        this.searchResultsQ.pause();
+
+        // push first page
+        this.searchResultsQ.push({ pageNumber: 1 }, () => this.kitaPagesQ.resume());
+        this.queuedPages.push(1);
 
         // define a q to process each result
-        const kitaPagesQ = queue(this.parseKitaPage, 4);
+        this.kitaPagesQ = queue<{kitaUrl:string, pageNumber:number}, object>((task:{kitaUrl:string, pageNumber:number}, callback) =>
+            this.getKitaPage(task.kitaUrl, task.pageNumber, callback), 4);
+        this.kitaPagesQ.pause();
     }
 
-    search(): Promise<[KitaResult]> {
+    search(): Promise<KitaResult[]> {
         return new Promise((resolve, reject) => {
-            const results: [KitaResult] = [];
-
-
-            q.pause();
-            q.drain = () => { resolve(results) };
-            let pageNumber = 1;
-            doWhilst((callback) => {
-                this.getPage(keyword, plz, city, land, pageNumber++)
-                    .then((pageResults:KitaNetzDEPageResults) => {
-                        if (pageResults.kitaLinks.length > 0) {
-                            pageResults.kitaLinks.forEach((url) => q.push({url}));
-                        }
-                        callback();
-                    });
-            }, (results) => results.last().hasNextPage)
+            this.searchResultsQ.resume();
+            this.kitaPagesQ.drain = () => { resolve(this.results) };
         });
 
     };
 
-    getPage(keyword: string, plz: number, city: string, land: KitaNetzLANDOpts, pageNumber: number): Promise<KitaNetzDEPageResults> {
+    getSearchPage(keyword:string, plz:number, city:string, land:KitaNetzLANDOpts, pageNumber:number, callback) {
         const options = {
-            uri: this.baseUrl,
+            uri: KitaNetzDE.baseUrl,
             qs: {
-                t_suchwort: keyword || "",
-                t_plz: plz || "",
-                t_stadt: city || "",
-                t_land: land || "",
+                t_suchwort: this.keyword || "",
+                t_plz: this.plz || "",
+                t_stadt: this.city || "",
+                t_land: this.land || "",
                 go: pageNumber
             },
             transform: body => cheerio.load(body)
         };
-        return new Promise((resolve, reject) => {
-            rp(options).then(($) => {
-                const links = $("#inhalt").children("a").filter((i, el) => {
-                    return $(el).attr("title") != "Home";
-                }).map((i, el) => {
-                    return path.join(this.baseUrl, $(el).attr("href"));
-                }).get();
-                const pagesLinks = $("#navigation").children("a");
-                const hasNextLink = pagesLinks.filter((i, el) => $(el).text() == " » ").length > 0;
-                return resolve(new KitaNetzDEPageResults(pageNumber, hasNextLink, links));
-            }).catch((err) => {
-                console.error(err);
-                reject(err);
-            });
+        // console.log(options);
+        rp(options).then(($) => this.parseSearchPage($, pageNumber, callback));
+    }
+
+    parseSearchPage($, pageNumber, callback) {
+        const links = $("#inhalt").children("a").filter((i, el) => {
+            return $(el).attr("title") != "Home";
+        }).map((i, el) => {
+            return KitaNetzDE.baseUrlForKita + $(el).attr("href").replace("../", "")
+                .replace("http:/www", "http://www");
+        }).get();
+        const pageNumbers = $("#navigation")
+            .children("a")
+            .filter((i, el) => $(el).text().trim() != "»" && $(el).text().trim() != "«")
+            .map((i, el) => parseInt($(el).text()))
+            .get();
+        links.forEach(kitaUrl => {
+            this.kitaPagesQ.push({ kitaUrl, pageNumber });
         });
+
+        pageNumbers.forEach((page) => {
+            if (this.queuedPages.indexOf(page) < 0) {
+                this.queuedPages.push(page);
+                this.searchResultsQ.push({ pageNumber: page });
+            }
+        });
+        callback();
+    }
+
+    getKitaPage(kitaUrl:string, pageNumber:number, callback) {
+        const options = { uri: kitaUrl, transform: body => cheerio.load(body) };
+        // console.log(kitaUrl, pageNumber);
+        rp(options).then(($) => this.parseKitaPage($, kitaUrl, pageNumber, callback));
+    }
+
+    parseKitaPage($, kitaUrl:string, pageNumber:number, callback) {
+        const main = $("main[role='main']").first();
+        const title = main.children("h1").first().text();
+        const address = main.find(".zeilen").first().text()
+            .replace("\n", " ")
+            .replace("<br>", " ")
+            .replace("\n", " ")
+            .trim();
+        console.log({
+            title,
+            address,
+            kitaUrl,
+            pageNumber
+        });
+        callback();
     }
 }
